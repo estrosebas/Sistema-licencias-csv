cf9302a4dea63d93b884851d4e8518786b214dd4
estrosebas <estrosebas@gmail.com>
2024-07-04 03:19:11 -0500

    subida

--- asistencia/src/main/java/TAD_QuickSort/OrdenarListaInterface.java
+++
@@ -1,8 +0,0 @@
-package TAD_QuickSort;
-
-import TAD_Establecimiento.ListaEnlazadaImplEstablecimiento;
-
-public interface OrdenarListaInterface {
-
-	public void ordenar(ListaEnlazadaImplEstablecimiento lista);
-}
